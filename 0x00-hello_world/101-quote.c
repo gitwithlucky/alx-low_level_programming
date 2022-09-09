a050242c765a1febc255152aca312a39289dc156
GitWithLucky <luckyikenkwocha@gmail.com>
2022-09-09 00:32:28 -0700

    c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main - Entry point
  *
- * Return: always 0
+ * Return: always 1
 */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
-	return (0);
+	write(2,stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
