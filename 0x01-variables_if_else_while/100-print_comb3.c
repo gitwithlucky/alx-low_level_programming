d5f87dd20e52f626b9dad885f6f198d98452ef1e
GitWithLucky <luckyikenkwocha@gmail.com>
2022-09-11 03:01:40 -0700

    C

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,14 +10,23 @@
 int main(void)
 {
 int i;
+int j;
 
-for (i = 10; i < 20; i++)
+for (i = 10; i <= 19; i++)
+{
+for (j = 10; j <= 19; j++)
+{
+if ((j % 10) > (i % 10))
 {
 putchar((i % 10) + '0');
-if (i != 19)
+putchar((j % 10) + '0');
+if (i != 18 || j != 19)
 {
 putchar(',');
 putchar(' ');
+}
+}
+
 }
 }
 putchar('\n');
