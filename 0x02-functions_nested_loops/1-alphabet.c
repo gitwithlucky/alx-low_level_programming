1b9b49b9c125f9e569931eb70a1b960ed2a3a9ac
GitWithLucky <luckyikenkwocha@gmail.com>
2022-09-14 23:51:50 -0700

    C

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,10 +6,10 @@
 */
 void print_alphabet(void)
 {
-	for(int i = 97; i < 123; i++)
+	int i;
+	for (i = 97; i < 123; i++)
 	{
 		_putchar(i);
 	}
 	_putchar('\n');
-	return (0);
 }
--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,9 +7,10 @@
 void print_alphabet_x10(void)
 {
 	int count = 0;
+	int i;
 	while (count != 10)
 	{
-		for(int i = 97; i < 123; i++)
+		for (i = 97; i < 123; i++)
 		{
 			_putchar(i);
 		}
