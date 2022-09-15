b33eee64eddb101d079a81396ce1bed730c50a08
GitWithLucky <luckyikenkwocha@gmail.com>
2022-09-15 07:58:09 -0700

    C

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,11 @@
+
 /**
- *
- * Entry point
- *
- * Return 0 or 1
- *
+ * _isupper - checks for uppercase character.
+ * @c: the character to be checked.
+ * Return: 1 if c is uppercase, 0 otherwise.
+ *   
  */
+
 
 int _isupper(int c)
 {
