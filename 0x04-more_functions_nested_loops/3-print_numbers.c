cd34fdb01555c84672681db83069aaa61868ad1d
GitWithLucky <luckyikenkwocha@gmail.com>
2022-09-15 20:35:13 -0700

    C

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,3 +1,5 @@
+#include "main.h"
+
 /**
  * print_numbers - prints the numbers, from 0 to 9, followed by a new line.
  */
@@ -13,5 +15,4 @@ while (num <= '9')
 _putchar(num);
 num++;
 }
-_putchar('\n');
-}
+
