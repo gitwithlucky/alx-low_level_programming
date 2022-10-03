04149096333f6bc21fa47e082a06aacc19d2e6de
GitWithLucky <luckyikenkwocha@gmail.com>
2022-10-03 00:40:10 -0700

    Argument Vectors

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#define UNUSED(x) (void)(x)
+
+/**
+ * main - main function
+ * @argc: argumentc
+ * @argv: vector of arguments
+ *Return: always 0
+ */
+
+int main(int argc, char  *argv[])
+{
+0-whatsmyname.c UNUSED(argv);
+0-whatsmyname.c printf("%d\n", argc - 1);
+0-whatsmyname.c return (0);
+}
