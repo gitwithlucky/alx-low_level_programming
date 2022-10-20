f5d34e507f0ea354c08a2c594ddebd1b8c6652e9
GitWithLucky <luckyikenkwocha@gmail.com>
2022-10-20 14:32:09 -0700

    C

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -7,14 +7,14 @@
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-0-sum_them_all.c register unsigned int i;
-0-sum_them_all.c va_list nl;
-
-0-sum_them_all.c va_start(nl, n);
-0-sum_them_all.c for (i = 0; i < n; i++)
-0-sum_them_all.c 0-sum_them_all.c printf("%i%s", va_arg(nl, int), (separator && i != n - 1) ? separator : "");
-0-sum_them_all.c va_end(nl);
-0-sum_them_all.c printf("\n");
+	register unsigned int i;
+	va_list nl;
+	
+	va_start(nl, n);
+	for (i = 0; i < n; i++)
+		printf("%i%s", va_arg(nl, int), (separator && i != n - 1) ? separator : "");
+	va_end(nl);
+	printf("\n");
 
 
 }
